fca74c74275dccab56385d520dfe41f1cc720f98
Javier Pitart <mpitartf01@educantabria.es>
2024-04-16 16:26:53 +0200

    Cuarta fase proyecto: Clase creada,comentada y codificada y Principal creadas y comentadas

--- DirectorioTrabajo_Pitartavier/src/Clase.java
+++ DirectorioTrabajo_Pitartavier/src/Clase.java
@@ -1,4 +1,7 @@
 
 public class Clase {
 	//Aqui se creará un System.out.println par imprimir un mensaje por consola
+	 public Clase() {
+	        System.out.println("Soy un objeto de la clase y acabo de ser creado");
+	    }
 }
